e12658a55792e50f94bcf21db287795de4e4c0ff
Ujg11 <126014096+Ujg11@users.noreply.github.com>
2023-05-09 23:16:32 +0200

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,43 @@
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int ft_atoi(const char *str)
+{
+    int i;
+    int neg;
+    int res;
+
+    i = 0;
+    neg = 1;
+    res = 0;
+    while (str[i] != '\0' && (str[i] == ' ' || str[i] == '\f'
+			|| str[i] == '\n' || str[i] == '\r' || str[i] == '\t'
+			|| str[i] == '\v'))
+		i++;
+    if (str[i] == '-')
+    {
+        neg = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+    return (res * neg);
+}
+/*
+int main(void)
+{
+    char c[] = " \f\n\r\t\v -34561";
+    int res;
+
+    res = atoi(c);
+    printf("Resultat atoi= %d\n", res);
+    res = ft_atoi(c);
+    printf("Resultat ft_atoi= %d\n", res);
+    return (0);
+}*/
