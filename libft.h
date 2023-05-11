cb1f0a1b0209e48b8b69d4a912e9c64879a2b1e3
Ujg11 <126014096+Ujg11@users.noreply.github.com>
2023-05-11 19:53:59 +0200

    Add files via upload

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ojimenez <ojimenez@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:07:10 by ojimenez          #+#    #+#             */
-/*   Updated: 2023/05/11 19:28:44 by ojimenez         ###   ########.fr       */
+/*   Updated: 2023/05/11 19:52:10 by ojimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ int		ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif
