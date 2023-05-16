8edc7da0b2d5b1e64130bb4ce2987b5c2d1f2adf
Oriol Jimenez Garrich <ojimenez@car13s5.42barcelona.com>
2023-05-16 15:26:44 +0200

    Fet strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,89 +6,13 @@
 /*   By: ojimenez <ojimenez@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 11:40:44 by ojimenez          #+#    #+#             */
-/*   Updated: 2023/05/16 15:21:11 by ojimenez         ###   ########.fr       */
+/*   Updated: 2023/05/16 15:25:37 by ojimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
-/*
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	char	*copy;
-	size_t	len;
-	size_t	i;
-
-	i = 0;
-	len = ft_strlen(s1) + 1;
-	copy = (char *)malloc(len * sizeof(char));
-	if (!copy)
-		return (NULL);
-	while (s1[i])
-	{
-		copy[i] = s1[i];
-		i++;
-	}
-	copy[i] = '\0';
-	return (copy);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*sub;
-	size_t	i;
-	size_t	fin;
-
-	fin = 0;
-	i = 0;
-	if (!s)
-		return (NULL);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	while (s[fin])
-		fin++;
-	if ((size_t)s + fin < len)
-		len = (size_t)s + fin;
-	sub = (char *)malloc((len + 1) * sizeof(char));
-	if (!sub)
-		return (NULL);
-	while (s[start + i] && i < len)
-	{
-		sub[i] = s[start + i];
-		i++;
-	}
-	sub[i] = '\0';
-	return (sub);
-}
-
-char	*ft_strchr(const char *str, int c)
-{
-	int		i;
-	char	*s;
-
-	i = 0;
-	s = (char *)str;
-	while (s[i])
-	{
-		if (s[i] == (char)c)
-			return ((char *)(s + i));
-		i++;
-	}
-	if ((char)c == '\0')
-		return ((char *)&str[i]);
-	return (NULL);
-}
-*/
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	ini;
