463453fb370323d3b610928b01db6067abb89bfb
Ujg11 <orioljg2002@gmail.com>
2023-05-18 00:31:28 +0200

    Bonus fins lstnew

---
+++ ft_lstadd_back.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ojimenez <ojimenez@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 22:23:59 by ojimenez          #+#    #+#             */
+/*   Updated: 2023/05/17 22:23:59 by ojimenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	last->next = new;
+	new->next = NULL;
+}
--- libft.h
+++ libft.h
@@ -14,6 +14,14 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+
+typedef struct	s_list
+{
+void			*content;
+struct s_list	*next;
+}				t_list;
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -49,10 +57,11 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-typedef struct	s_list
-{
-void			*content;
-struct s_list	*next;
-}				t_list;
+//Part Bonus
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
